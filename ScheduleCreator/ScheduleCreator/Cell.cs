f995b8ad3481e5089fc5bb07d93a6377597a476e
Ulatel <ulatlene@mail.ru>
2022-09-23 21:33:11 +0700

    Elevement

---
+++ ScheduleCreator/ScheduleCreator/Cell.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Cell
+    {
+        private bool busy = false;
+
+        public bool isFree()
+        {
+            if (busy == true)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ScheduleCreator/ScheduleCreator/Element.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Element
+    {
+        protected string name;//фамилия преподавателя/номер группы/номер аудитории(для дочерних элементов)
+        protected Cell[] Workload;//массив 6 на 7 (6 дней и каждый день максимум 7 пар)
+
+    }
+}
