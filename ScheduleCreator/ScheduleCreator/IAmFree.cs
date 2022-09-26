572c5f842c215dbd641ebd7b3379eb7e4d7fd9d5
Ulatel <ulatlene@mail.ru>
2022-09-26 22:04:23 +0700

    update classes

---
+++ ScheduleCreator/ScheduleCreator/IAmFree.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScheduleCreator
+{
+    interface IAmFree
+    {
+        void IsFree(int day, int lesson);
+        void isFree(int day, int lesson);
+    }
+}
--- ScheduleCreator/ScheduleCreator/Teacher.cs
+++ ScheduleCreator/ScheduleCreator/Teacher.cs
@@ -4,11 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace test
+namespace ScheduleCreator
 {
    
     internal class Teacher : Element
     {
-
+        public bool IsFree(int day, int lesson)
+        {
+            if (day >= 0 and day<6 and lesson >= 0 and lesson<7)
+                return isFree(day, lesson)
+            else Console.WriteLine("Ошибка ввода даты или номера пары");
+            return false;
+        }
+        protected bool isFree(int day, int lesson)
+        {
+            if (Cell[day][lesson] == true);
+            return true
+                else return false;
+        }
     }
 }
