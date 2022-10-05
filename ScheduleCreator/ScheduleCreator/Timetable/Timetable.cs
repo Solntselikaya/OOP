b9a10432059b9bc77d070d005e1bfb5f2da23482
Dim-ulik <d.martyshev@yandex.ru>
2022-10-05 20:19:08 +0700

    NEW TimeTABLES!!!!!!

--- ScheduleCreator/ScheduleCreator/Timetable/Timetable.cs
+++ ScheduleCreator/ScheduleCreator/Timetable/Timetable.cs
@@ -8,16 +8,47 @@ namespace ScheduleCreator
 {
     internal class Timetable
     {
-        private Lesson[,] timetable;
+        private Study[6,7] timetable;
 
         private void GenerateTimetable(List<StudyPlan> studyPlanList)
         {
-
+            for(int i = 0; i < studyPlanList.Count; i++)
+            {
+                for(int k = 0; k < MainActivity.InputTeachers; k++)
+                {
+                    if (MainActivity.InputTeachers[k] == studyPlanList[i].listOfLessons.Item1.teacher)
+                    {
+                        FindPlaceInTable(MainActivity.InputTeachers[k], studyPlanList[i].listOfLessons);
+                        break;
+                    }
+                }
+            }
         }
 
-        private void CheckIfElementIsFree(IElement element, int first, int second)
+        private int FindPlaceInTable(Teacher teacher, List<Tuple<Lesson, LType>> lessonsList)
         {
-
+            for (int x = 0; x < 6; x++)
+            {
+                for (int y = 0; y < 7; y++)
+                {
+                    if (teacher.IsFree(x, y))
+                    {
+                        for(int i = 0; i < MainActivity.InputClassrooms.Count; i++)
+                        {
+                            if (MainActivity.InputClassrooms[i].IsFree(x, y))
+                            {
+                                MainActivity.InputClassrooms[i].DoBusy(x, y);
+                                teacher.DoBusy(x, y); 
+                                timetable[x, y].name = lessonsList.Item1.lesson.name;
+                                timetable[x, y].classrom = MainActivity.InputClassrooms[i].name;
+                                timetable[x, y].teacher = teacher.name;
+                                timetable[x, y].lessonType = lessonsList.Item2;
+                                return 0;
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
