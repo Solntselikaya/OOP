352ce56ac0d58b70dc3bb0d96839138bf14cb381
Necusic <97069468+Necusic@users.noreply.github.com>
2022-10-05 22:30:57 +0700

    Create MainActivity
    Создал майн активи

---
+++ ScheduleCreator/ScheduleCreator/MainActivity.cs
@@ -0,0 +1,27 @@
+namespace ScheduleCreator
+{
+    public class  MainActivity{
+
+        private Scheddule scheddule;
+        public List<StudyPlan> inputStudyPlans;
+        public List<ClassRoom> inputClassrooms;
+        public List<Teacher> inputTeachers;
+
+        public void GetTeachers(List<Teacher> inputTeachers)
+        {
+            inputTeachers.Add(Console.ReadLine);
+            
+        }
+
+        public void GetClassRooms(List<ClassRoom> inputClassrooms)
+        {
+            inputClassrooms.Add(Console.ReadLine);
+        }
+
+        public void GetStudyPlans(List<StudyPlan> inputStudyPlans)
+        {
+            inputStudyPlans.Add(Console.ReadLine);
+        }
+ 
+    }
+}
